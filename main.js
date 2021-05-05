f5fd15f526063c998cb8c4ec658427c5b8b2a968
marilyn-n <negrete.aguilar.m@gmail.com>
2021-05-05 14:37:53 -0500

    add fadeToggling effects

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 $(document).ready(function() {
+    //$(document).ready(function() {} <----- line 3 This works same as line 4
+    //$(function() {} <---- Shorter way to call document ready event
 
     $('#square').text('hi');
     $('#square').click(randomBackground);
     $('#square').click(randomBorderThickness);
+
 
     function randomBackground() {
         const red = Math.floor(Math.random() * 255);
@@ -17,6 +20,21 @@ $(document).ready(function() {
     function randomBorderThickness() {
         const randomNumer = Math.floor(Math.random() * 10);
         $('#square').css('border-width', randomNumer);
+    }
+
+    $('.btn-effect').click(fadeToggling);
+    let divVisible = true;
+
+    function fadeToggling() {
+        if(divVisible) {
+            $('#rectangle').fadeOut(1000);
+            //fadeOut(1000) it will fadeOut in 1 second
+            divVisible = !divVisible; // changes divVisible to false after div fades out
+        } else {
+            $('#rectangle').fadeIn(1000);
+            //fadeIn(1000) it will fadeIn in 1 second
+            divVisible = !divVisible; // !divVisible reverses the value of divVisible to true
+        }
     }
 
 
